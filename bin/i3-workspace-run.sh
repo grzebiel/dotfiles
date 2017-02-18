f9191dd3d720d34340d0fba2205b8f2c9bab961a
grzebiel <grzebieluch@me.com>
2017-02-18 21:41:37 +0100

    i3 workspace helper script added

---
+++ bin/i3-workspace-run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#usage i3-workspace-run.sh [workspace] [application]
+#focus window class $3 if process works. Otherwise go to specyfic workspace and launch it
+echo "$1,$2,$3"
+if pidof $2 2>&1 > /dev/null;
+then
+    i3-msg -q [class=$3] focus
+else
+    i3-msg -q workspace $1
+    $2
+fi
