b311d93cdf41c6bc436a7fe77ad78ec385994cc9
grzebiel <grzebieluch@me.com>
2019-10-04 23:44:53 +0200

    permament display settings

--- .config/rofi/rofi-displays.sh
+++ .config/rofi/rofi-displays.sh
@@ -4,7 +4,13 @@ layout_dir="$HOME/.screenlayout"
 
 if [ -z $@ ]
 then
-    ls $layout_dir
+    ls $layout_dir | grep -v current
+    echo new
 else
+    if [ $1 == new ]
+    then
+        coproc arandr
+    fi
+    ln -sf "$layout_dir/$1" "$layout_dir/current"
     exec "$layout_dir/$1"
 fi
