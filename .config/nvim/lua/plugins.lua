592c3dddab23655c74c7004f0666b2731378b258
grzebiel <grzebieluch@me.com>
2023-11-07 11:05:18 +0100

    up from loki

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -114,8 +114,21 @@ return require('packer').startup(function(use)
     config = function() require "configs/toggleterm" end,
   }
 
+  use {
+  "folke/noice.nvim",
+  config = function() require "configs/noice" end,
+  requires = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+    }
+  }
+
   -- openai interface
-  use 'madox2/vim-ai'
+  -- use 'madox2/vim-ai'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
